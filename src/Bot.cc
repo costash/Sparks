ab62cf2b0d602c91c701bd72f4c2878f068a9dbf
Parcalabescu Maria <parcalabescu_maria@yahoo.com>
2012-02-29 21:23:22 +0200

    Basic exploreFood, not final version.

--- src/Bot.cc
+++ src/Bot.cc
@@ -32,7 +32,7 @@ void Bot::makeMoves()
     state.bug << state << endl;
 
     //picks out moves for each ant
-    for(int ant = 0; ant < (int) state.myAnts.size(); ant++)
+   /* for(int ant = 0; ant < (int) state.myAnts.size(); ant++)
     {
         int d = rand() % TDIRECTIONS;
         Location loc = state.getLocation(state.myAnts[ant], d);
@@ -43,7 +43,8 @@ void Bot::makeMoves()
             loc = state.getLocation(state.myAnts[ant], d);
         }
         state.makeMove( state.myAnts[ant], d );
-    }
+    }*/
+    explore();
 
     state.bug << "time taken: " << state.timer.getTime() << "ms" << endl << endl;
 };
@@ -53,35 +54,52 @@ void Bot::explore()
 {
     Location foodLoc, currentLoc, newLoc;
     std::queue<Location> foodQueue;
-    
+    std::vector<std::vector<bool> > visited( state.rows, std::vector<bool>(state.cols, 0) );
+       
     for(int i = 0; i < (int) state.food.size(); ++i)
     {
         foodLoc = state.food[i];
         foodQueue.push(foodLoc);
-        
-        std::vector<std::vector<bool> > visited( state.rows, std::vector<bool>(state.cols, 0) );
-        
         visited[foodLoc.row][foodLoc.col] = 1;
-        
-        while ( !foodQueue.empty() ) {
-            currentLoc = foodQueue.front();
-            foodQueue.pop();
-            
-            for ( int d = 0; d < TDIRECTIONS; ++d )
-            {
-                newLoc = state.getLocation(currentLoc, d);
-                
-                if (!visited[newLoc.row][newLoc.col])
-                {
-                    //TODO Insert in a matrix here, the level...
-                    foodQueue.push(newLoc);
-                }
-                
-                visited[newLoc.row][newLoc.col] = 1;
-            }
-            
-        }
     }
+    
+    while ( !foodQueue.empty() ) 
+    {
+        currentLoc = foodQueue.front();
+        foodQueue.pop();
+            
+        for ( int d = 0; d < TDIRECTIONS; ++d )
+        {
+            newLoc = state.getLocation(currentLoc, d);
+                
+            if (!visited[newLoc.row][newLoc.col])
+            	if(state.grid[newLoc.row][newLoc.col].isVisible)
+            	{
+	            	if(!state.grid[newLoc.row][newLoc.col].isWater &&
+	            	   state.grid[newLoc.row][newLoc.col].ant == -1	)
+            		{
+                    //TODO Insert in a matrix here, the level...
+                		foodQueue.push(newLoc);
+                		visited[newLoc.row][newLoc.col] = 1;
+            		}
+            		
+            		if(state.grid[newLoc.row][newLoc.col].ant == 0)	//our ant is here
+            		{
+            			int antDir;
+            			switch(d)
+            			{
+            				case 0: antDir = 2;break;
+            				case 1: antDir = 3;break;
+            				case 2: antDir = 0;break;
+            				case 3: antDir = 1;break;
+            			}
+            			state.makeMove( newLoc, antDir);
+            			//TODO: Find a way to stop if every piece of food has an ant coming
+            		}
+        	}
+	}
+    }
+    
 }
 
 //finishes the turn
