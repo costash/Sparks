6072f39c00e46fd74225636f61348afe959ab472
Constantin Serban-Radoi <costashsrc@gmail.com>
2012-03-01 23:23:08 +0200

    Added a run.sh script to run with 32-bit bot on cell_maze_p02_19.map

---
+++ lin/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+./playgame.py --player_seed 0 --engine_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 100 --map_file maps/cell_maze/cell_maze_p02_19.map "$@" "./bot32" "./MyBot"
